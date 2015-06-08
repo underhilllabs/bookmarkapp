b9eb780085595f80bdd4c6774a90e4ab4ab67fba
Bart Lantz <bartlantz@gmail.com>
2015-06-08 13:58:29 -0600

    adds android platform

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,23 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.borismus.webintent/www/webintent.js",
+        "id": "com.borismus.webintent.WebIntent",
+        "clobbers": [
+            "WebIntent"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.borismus.webintent": "1.0.0",
+    "cordova-plugin-whitelist": "1.0.0"
+}
+// BOTTOM OF METADATA
+});
